1a7631c4cee2e0823bb61d59aaf74d79be347b7b
Arnaud Rivero <rivero.arnaud@gmail.com>
2014-11-26 09:16:10 +0100

    maj

--- creation.sql
+++ creation.sql
@@ -1,14 +1,96 @@
+/*Remise à zéro de la base de donnée*/
 drop database commerce;
 
 create database commerce;
 
 use commerce;
 
-create table produit(
-       reference int primary key auto_increment,
+/*Creation des tables*/
+
+create table if not exists produit(
+       id_produit int primary key auto_increment,
        designation varchar(64) unique not null,
        descriptif text, 
-       quantite int not null, check (quantite >= 0),
+       quantite int unsigned not null, check (quantite >= 0),
        prix float not null, check (prix >=0 ),
        photo varchar(255),
        enVente boolean not null default false);
+
+create table if not exists catalogue(
+       id_catalogue int primary key auto_increment, 
+       nom varchar(64) not null unique, 
+       dateMAJ date);
+
+create table if not exists reference(
+       id_catalogue int, 
+       id_produit int,
+       primary key(id_catalogue, id_produit),
+       foreign key (id_catalogue) references catalogue(id_catalogue), 
+       foreign key (id_produit) references produit(id_produit));
+
+create table if not exists ville(
+       id_ville int primary key auto_increment, 
+       nom varchar(64) not null, 
+       CP int unsigned not null);
+
+create table if not exists membre(
+       id_membre int primary key auto_increment, 
+       nom varchar(64) not null, 
+       prenom varchar(64) not null, 
+       adresse varchar(64) not null, 
+       mail varchar(64) not null unique, 
+       tel varchar(10) not null, 
+       mdp varchar(20) not null, 
+       id_ville int not null, 
+       foreign key (id_ville) references ville(id_ville),
+       check (mail like '%@%\.%'), 
+       check (char_length(tel) = 10));
+
+create table if not exists panier(
+       id_membre int, 
+       id_produit int,
+       quantite int unsigned not null,
+       primary key(id_membre, id_produit),
+       foreign key (id_membre) references membre(id_membre), 
+       foreign key (id_produit) references produit(id_produit),
+       check (quantite > 0));
+
+create table if not exists commande(
+       id_commande int primary key auto_increment, 
+       dateValidation date not null, 
+       dateLivraison date not null, 
+       adrLivraison varchar(64),
+       fraisDePort int unsigned not null,
+       id_ville int not null,
+       id_membre int not null, 
+       foreign key (id_ville) references ville(id_ville),
+       foreign key (id_membre) references membre(id_membre), 
+       check (dateValidation <= now()),
+       check (dateValidation <= dateLivraison));
+
+create table if not exists contenir(
+       id_commande int,
+       id_produit int, 
+       quantite int unsigned not null, 
+       PUCommande int unsigned not null, 
+       primary key (id_commande, id_produit), 
+       foreign key (id_commande) references commande(id_commande), 
+       foreign key (id_produit) references produit(id_produit));
+
+create table if not exists avis(
+       id_membre int, 
+       id_produit int, 
+       note tinyint unsigned not null,
+       commentaire text, 
+       dateAvis date not null, 
+       primary key (id_membre, id_produit), 
+       foreign key (id_membre) references membre(id_membre), 
+       foreign key (id_produit) references produit(id_produit),
+       check (note <= 5), 
+       check (dateAvis <= now()));
+
+/* Creation des triggers */
+
+/* Creation des vues */
+
+/* Creation des index */
