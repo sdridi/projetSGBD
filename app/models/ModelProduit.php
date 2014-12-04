ed6bdec8201769b9f8542be43d2408e630e4fe5b
Sami DRIDI <sdridi@enseirb-matmeca.fr>
2014-12-04 20:31:31 +0100

    rajout de fichier

---
+++ app/models/ModelProduit.php
@@ -0,0 +1,28 @@
+
+    
+	<h1>
+		Produit</h1>
+		<?php
+		require"Model.php";
+		require"Db.php";
+		Class ModelProduit extends Model {
+			public function __construct()
+			{
+				$this->_db = Db::init();
+				$table = "produit";
+			}				
+
+
+      public function add($values){
+        $reference = $this->_db->exec("INSERT INTO produit (designation, descriptif, quantite, prix, photo, enVente) VALUES $values ");
+        echo "bonjour";
+    } 
+
+     
+
+
+    public function delete($reference){
+                $reference = $_db->exec("DELETE FROM produit WHERE id_produit='$reference'");
+          } 
+
+}
