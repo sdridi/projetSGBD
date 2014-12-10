da9c1d00417b65bf7e4f7f1cfcb82f85b9a8faf0
Arnaud Rivero <rivero.arnaud@gmail.com>
2014-12-10 10:16:52 +0100

    requetes commande

--- requetes.sql
+++ requetes.sql
@@ -118,6 +118,10 @@ begin
 end $$
 
 /************** MEMBRES, ADMINS & VILLES **************/
+create procedure get_ville (IN ref int)
+begin
+	select * from ville where id_ville = ref;
+end $$
 
 create procedure add_ville (IN nom_ville varchar(64),
        		 	    IN CP_ville int unsigned)
@@ -137,6 +141,23 @@ end $$
 create procedure delete_ville (IN ref int)
 begin
 	delete from ville where id_ville = ref;
+end $$
+
+create procedure get_membre (IN ref int)
+begin
+	select nom, prenom, adresse, mail, tel, id_ville from membre where id_membre = ref;
+end $$
+
+/* Vérifie que le membre a fourni un couple (mail, mdp) valide 
+   Si le couple est valide, l'identifiant du membre est retourné
+   sinon 0 est retourné */
+create procedure check_mdp_membre (IN mail_membre varchar(64), 
+       		 		   IN mdp_membre varchar (20))
+begin
+	select if (exists(select id_membre from membre where mail = mail_membre
+	       	  		 	   	       and mdp = mdp_membre),
+		   id_membre, 0) as isValid
+	       from membre where mail = mail_membre;
 end $$
 
 create procedure create_membre (IN nom_membre varchar(64), 
@@ -176,6 +197,12 @@ begin
 		      where id_membre = ref;
 end $$
 
+create procedure update_membre_mdp (IN ref int, 
+       		 		    IN mdp_membre varchar(64))
+begin
+	update membre set mdp = mdp_membre where id_membre = ref;
+end $$
+
 create procedure delete_membre (IN ref int)
 begin
 	delete from membre where id_membre = ref;
@@ -183,15 +210,203 @@ begin
 	delete from commande where id_membre = ref;
 end $$
 
-/*create_admin
-update_admin
-delete admin
-update_droits_admin*/
+create procedure get_admin (IN ref int)
+begin
+	select username, droits, mail from admin where id_admin = ref;
+end $$
+
+/* Vérifie que l'admin a fourni un couple (username, mdp) valide 
+   Si le couple est valide, l'identifiant de l'admin est retourné
+   sinon 0 est retourné */
+create procedure check_admin_mdp (IN username_admin varchar(64), 
+       		 		  IN mdp_admin varchar(64))
+begin 
+      	select if (exists(select id_admin from admin where username = username_admin
+	       	  		 	   	       and mdp = mdp_admin),
+		   id_admin, 0) as isValid
+	       from admin where username = username_admin;
+end $$
+
+create procedure create_admin (IN username_admin varchar(64), 
+       		 	       IN droits_admin tinyint, 
+			       IN mdp_admin varchar(64), 
+			       IN mail_admin varchar(64))
+begin
+	insert into admin(username, droits, mdp, mail) values (username_admin,
+	       	    		    	    	       	       droits_admin,
+							       mdp_admin, 
+							       mail_admin);
+end $$
+
+create procedure update_admin (IN ref int, 
+       		 	       IN username_admin varchar(64), 
+       		 	       IN droits_admin tinyint, 
+			       IN mdp_admin varchar(64), 
+			       IN mail_admin varchar(64))
+begin
+	update admin set username = username_admin, 
+	       	     	 droits = droits_admin, 
+			 mdp = mdp_admin,
+			 mail = mail_admin
+		     where id_admin = ref;
+end $$
+
+create procedure delete_admin (IN ref int)
+begin
+	delete from admin where id_admin = ref;
+end $$
+
+create procedure update_droits_admin (IN ref int, 
+       		 		      IN droits_admin tinyint)
+begin
+	update admin set droits = droits_admin where id_admin = ref;
+end $$
+
 /************** PANIERS **************/
+create procedure get_panier(IN ref_membre int)
+begin
+	select * from panier natural join produit where id_membre = ref_membre;
+end $$
+
+/* Ajoute un article au panier */
+create procedure add_to_panier (IN ref_membre int, 
+       		 		IN ref_produit int, 
+				IN qte int unsigned)
+begin
+	insert into panier(id_membre, id_produit, quantite) values (ref_membre, ref_produit, qte);
+end $$
+
+/* Retire un article du panier */
+create procedure delete_from_panier (IN ref_membre int, 
+       		 		     IN ref_produit int)
+begin
+	delete from panier where id_membre = ref_membre and id_produit = ref_produit;
+end $$
+
+/* Modifie la quantité achetée d'un article */
+create procedure update_quantite_panier (IN ref_membre int, 
+       		 			 IN ref_produit int, 
+					 IN qte int unsigned)
+begin
+	update panier set quantite = qte where id_membre = ref_membre and id_produit = ref_produit;
+end $$
+
+/* Vide le panier d'un client */
+create procedure empty_panier (IN ref_membre int)
+begin
+	delete from panier where id_membre = ref_membre;
+end $$
 
 /************** COMMANDES **************/
+create procedure get_commande (IN ref_commande int)
+begin
+	select * from commande where id_commande = ref_commande;
+end $$
+
+/* Retourne l'ensemble des produits concernés par une commande, 
+   ainsi que l'identifiant des promos s'y appliquant lors de la commande */
+create procedure get_commande_produits_promos (IN ref_commande int, 
+       		 			       IN date_commande date)
+begin
+	select id_produit, id_promo_produit as id_promo, 'PRODUIT' as type
+	from concerne_promo_produit natural join promo_produit
+	     			    natural join reduction
+	where date_debut < date_commande and date_fin > date_commande
+	union
+	select id_produit, id_promo_catalogue as id_promo, 'CATALOGUE' as type
+	from produit_promo_catalogue
+	where date_debut < date_commande and date_fin > date_commande;
+end $$
+
+/* Retourne le prix unitaire des articles d'une commande lorsque celle-ci a été validée
+   ainsi que la quantité d'article acheté */
+create procedure get_commande_produits_PU_quantite (IN ref_commande int)
+begin
+	select id_produit, quantite, PUCommande
+	from contenir
+	where id_commande = ref_commande;
+end $$
+
+/* Retourne l'ensemble des commande d'un membre */
+create procedure get_commandes (IN ref_membre int)
+begin
+	select id_commande
+	from commande
+	where id_membre = ref_membre;
+end $$
+
+/* Met à jour toutes les quantités de produit après la validation d'une commande
+
+   /!\ PROCEDURE UTILISEE UNIQUEMENT PAR validate_commande, 
+       ET NON PAS PAR L'UTILISATEUR DE LA BASE DE DONNEE*/
+create procedure update_quantite_commande (IN ref_commande int, 
+       		 			   IN ref_membre int )
+begin
+	/* Fonctionnement : 
+	   On crée un curseur sur une requete récupérant tous les
+	   produits et leurs quantités dansa le panier du membre. 
+	   Tant que le curseur pointe sur un produit, il met à jour
+	   les quantités de ce dernier dans produits, grace à une
+	   boucle faire...tant que. 
+	   Une fois qu'il a fini, le curseur est fermé*/
+
+	declare done int default 0; /* condition d'arret */
+	declare id_prod int; /* variable stockant l'id du produit */
+	declare qte int; /* variable stockant la quantité du produit acheté */
+	declare cur cursor for select id_produit, quantite from panier where id_membre = ref_membre; /* curseur servant à la boucle for */
+	declare continue handler for not found set done = 1; /* handler passant la condition d'arret à 1 quand tous les produits ont été traités*/
+
+	open cur;
+
+	repeat
+		fetch cur into id_prod, qte; /*stockage de la ligne dans id_prod et qte*/
+		if not done then /*vérification que done != 1 , dans le cas où le panier est vide*/
+		   call update_produit_quantite(id_prod, qte); /*Mise à jour de la quantité de produit*/
+		end if;
+	until done end repeat; /* fin de la boucle faire... tant que*/
+
+	close cur;
+end $$
+
+/* Valide la commande d'un membre à partir de son panier 
+   La mise à jour des quantités n'est pas gérée et doit 
+   etre faite dans le code se servant de la base de donnée*/
+create procedure validate_commande (IN ref_membre int, 
+       		 		    IN dateLivr date, 
+				    IN adrLivr varchar(64), 
+				    IN fdp int unsigned, 
+				    IN id_ville_membre int)
+begin
+	/*Creation de la commande*/
+	insert into commande(dateValidation, dateLivraison, adrLivraison, fraisDePort, id_ville, id_membre)
+	       	    values (now(), dateLivr, adrLivr, fdp, id_ville_membre, ref_membre);
+	
+
+	/*Recuperation de l'id de la commande*/
+	select id_commande into @id_command from commande where id_membre = ref_membre order by 1 desc limit 1;
+	
+	/*Transfert du panier vers la commande*/
+	insert into contenir(id_commande, id_produit, quantite, PUCommande)
+	       	    select @id_command, id_produit, quantite, prix 
+		    from panier natural join produit
+		    where id_membre = ref_membre;
+
+	/* Mise à jour des quantités */
+	call update_quantite_commande(@id_command, ref_membre);
+
+	/*Vidage du panier*/
+	call empty_panier(ref_membre);
+end $$
 
 /************** AVIS **************/
+/*get_avis
+add_avis
+delete_avis*/
+
 
 /************** REDUCTIONS & PROMOS **************/
+/*get_promo
+create_promo
+delete_promo*/
+
 delimiter ;
